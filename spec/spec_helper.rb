b630cdabe44718f4212110be6fecf1ba5c1cfbaa
Ryan <ryanchan001@yahoo.com>
2014-08-17 23:41:05 -0700

    finish static

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,38 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  config.include Capybara::DSL
+end
+
+
+
+
+require 'spec_helper'
+
+describe "Static pages" do
+    
+    describe "Home page" do
+        
+        it "should have the content 'Sample App'" do
+            visit '/static_pages/home'
+            expect(page).to have_content('Sample App')
+        end
+    end
+    
+    describe "Help page" do
+        
+        it "should have the content 'Help'" do
+            visit '/static_pages/help'
+            expect(page).to have_content('Help')
+        end
+    end
+    
+    describe "About page" do
+        
+        it "should have the content 'About Us'" do
+            visit '/static_pages/about'
+            expect(page).to have_content('About Us')
+        end
+    end
 end
